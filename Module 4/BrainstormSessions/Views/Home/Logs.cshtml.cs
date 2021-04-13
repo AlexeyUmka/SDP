332a7d88876d0e1d2b14ca915392dd4d9c3c3283
KYIV\Oleksii_Yurchenko <Oleksii_Yurchenko@epam.com>
2021-04-13 22:35:19 +0300

    initial state

---
+++ Module 4/BrainstormSessions/Views/Home/Logs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BrainstormSessions.Views.Home
+{
+    public class Logs : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
