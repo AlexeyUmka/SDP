5a3871830855c9a95d93da36d60b8c89ddd7ebb5
KYIV\Oleksii_Yurchenko <Oleksii_Yurchenko@epam.com>
2021-05-05 21:43:20 +0300

    [Task 6.1] Fix search

--- Module 6/module6_task1.sql
+++ Module 6/module6_task1.sql
@@ -5,8 +5,8 @@ ALTER PROCEDURE sp_executesqlVariant @FieldName NVARCHAR(100), @FieldValue NVARC
 AS
 IF @FieldName NOT IN('FirstName', 'LastName', 'BirthDate')
 	THROW 50403, 'Passed field is not allowed', 1;  
-DECLARE @sql AS NVARCHAR(500) = N'SELECT FirstName, LastName, BirthDate FROM delivery.Driver WHERE ' + @FieldName + '=@FValue'
-EXEC sp_executesql @sql, N'@FValue AS NVARCHAR(100)', @FValue = @FieldValue
+DECLARE @sql AS NVARCHAR(500) = N'SELECT FirstName, LastName, BirthDate FROM delivery.Driver WHERE ' + @FieldName + ' LIKE ''%' + @FieldValue + '%'''
+EXEC sp_executesql @sql
 GO
 EXEC sp_executesqlVariant @FieldName = 'FirstName', @FieldValue = 'John'
 
@@ -15,7 +15,7 @@ ALTER PROCEDURE executeVariant @FieldName NVARCHAR(100), @FieldValue NVARCHAR(10
 AS
 IF @FieldName NOT IN('FirstName', 'LastName', 'BirthDate')
 THROW 50403, 'Passed field is not allowed', 1;  
-DECLARE @sql AS NVARCHAR(500) = 'SELECT FirstName, LastName, BirthDate FROM delivery.Driver WHERE ' + @FieldName + '=' + char(39) + @FieldValue + char(39)
+DECLARE @sql AS NVARCHAR(500) = 'SELECT FirstName, LastName, BirthDate FROM delivery.Driver WHERE ' + @FieldName + ' LIKE ''%' + @FieldValue + '%'''
 EXEC (@sql)
 GO
 EXEC executeVariant @FieldName = 'FirstName', @FieldValue = 'John'
