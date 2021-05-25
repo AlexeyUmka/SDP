527cb13f2d9876528876681c4ae894acc17ec77c
KYIV\Oleksii_Yurchenko <Oleksii_Yurchenko@epam.com>
2021-05-25 23:32:14 +0300

    add disconnected repository

--- Module 8/ADO_Practice/ConsoleApp/Program.cs
+++ Module 8/ADO_Practice/ConsoleApp/Program.cs
@@ -2,6 +2,7 @@
 using System.Data.Common;
 using System.Data.SqlClient;
 using System.IO;
+using System.Linq;
 using DAL;
 using DAL.Factories;
 using DAL.Interfaces;
@@ -22,25 +23,46 @@ namespace ConsoleApp
                 .Build();
             var serviceProvider = new ServiceCollection()
                 .AddScoped<IUnitOfWork, UnitOfWork>()
-                .AddTransient(typeof(IRepository<>), typeof(AdoConnectedRepository<>))
+                .AddTransient(typeof(IRepository<>), typeof(AdoDisconnectedRepository<>))
                 .AddSingleton<IDbReaderMapperFactory, DbReaderMapperFactory>()
                 .AddSingleton(configuration)
                 .BuildServiceProvider();
-            
-            using var unitOfWork = serviceProvider.GetService<IUnitOfWork>();
+
+            var unitOfWork = serviceProvider.GetService<IUnitOfWork>();
             var repository = serviceProvider.GetService<IRepository<Customer>>();
-            // repository.Insert(new Customer(){Id = 11, FirstName = "ggg", LastName = "ggg"});
-            // repository.Update(new Customer(){Id = 11, FirstName = "sss", LastName = "sss"});
-            // unitOfWork.Rollback();
-            // unitOfWork.CommitTransaction();
-            // repository.GetByKey(new {Id = 11});
-            // repository.Delete(new {Id = 11});
-            // unitOfWork.Save();
-            // unitOfWork.Save();
+            Console.WriteLine("Before Insert");
+            DisplayAll(repository);
+            repository.Insert(new Customer(){Id = 12, FirstName = "ggg", LastName = "ggg", Phone = "11"});
+            Console.WriteLine("After Insert");
+            DisplayAll(repository);
+            Console.WriteLine("Before Update");
+            unitOfWork.Save();
+            DisplayAll(repository);
+            repository.Update(new Customer(){Id = 12, FirstName = "sss", LastName = "sss", Phone = "22"});
+            Console.WriteLine("After Update");
+            DisplayAll(repository);
+            unitOfWork.Rollback();
+            var r = repository.GetByKey(new {Id = 12}).ToList();
+            Console.WriteLine("get by key 12");
+            DisplayCustomer(r.Single());
+            Console.WriteLine("Before Delete");
+            DisplayAll(repository);
+            repository.Delete(new {Id = 12});
+            Console.WriteLine("After Delete");
+            DisplayAll(repository);
+        }
+
+        private static void DisplayAll(IRepository<Customer> repository)
+        {
             foreach (var c in repository.GetAll())
             {
-                Console.WriteLine($"{c.Id}, {c.FirstName}, {c.LastName}, {c.Phone}");
+                DisplayCustomer(c);
             }
+        }
+
+        private static void DisplayCustomer(Customer c)
+        {
+            Console.WriteLine($"{c.Id}, {c.FirstName}, {c.LastName}, {c.Phone}");
         }
     }
 }
