132417cb480855c524b0f859033d328cd6b421de
KYIV\Oleksii_Yurchenko <Oleksii_Yurchenko@epam.com>
2021-06-05 10:37:56 +0300

    [Task 7.2] Add comment to the script

--- Module 7/Task_2.sql
+++ Module 7/Task_2.sql
@@ -1,11 +1,13 @@
 USE DeliveryDB
 DELETE FROM delivery.Route WHERE OriginWarehouseId = 2 AND DestinationWarehouseId = 9;
+/*Run this part as a separate request to test paralel work of two operations and to test that isolation level was chosen properly*/
 SET TRANSACTION ISOLATION LEVEL REPEATABLE READ;
 BEGIN TRANSACTION Connection1;
 	SELECT *
 	FROM delivery.Route 
 	WHERE OriginWarehouseId = 2 AND DestinationWarehouseId = 7;
 COMMIT TRANSACTION;
+/*End of the part*/
 BEGIN TRANSACTION Connection2;
 	UPDATE delivery.Route
 	SET DestinationWarehouseId = 9
